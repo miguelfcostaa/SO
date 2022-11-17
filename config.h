5ed67ae7aa1953ce03547d3a77410eba59b1746c
2105319 <inesjardim.4@gmail.com>
2022-11-17 14:55:34 +0000

    asd

--- config.h
+++ config.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define UNIXSTR_PATH "/tmp/s.unixstr"
+#define UNIXDG_PATH  "/tmp/s.unixdgx"
+#define UNIXDG_TMP   "/tmp/dgXXXXXXX"
 
 struct pessoa
 {
